36eb63a7ee265c11662de26be9b7bbf942b3cef1
salsabeelnoor <salsabeelnoor@gmail.com>
2022-11-08 00:51:00 +0600

    mongodb connection and env is set

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = 5000; //process.env.PORT ||
@@ -7,6 +9,17 @@ const port = 5000; //process.env.PORT ||
 //middleware
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jvabeue.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 app.get("/", (req, res) => {
   res.send("Bridal Makeover Server Running");
